592b0219640cb8e49933f5b8a7847bfed597442b
Duvanav85 <duvanav85@yahoo.es>
2026-07-03 19:32:34 -0500

    update 7

--- api/index.mjs
+++ api/index.mjs
@@ -1,4 +1,4 @@
-﻿export default async function handler(req, res) {
+export default async function handler(req, res) {
   const { reqHandler } = await import('../dist/AgroInter/server/server.mjs');
   return reqHandler(req, res);
 }
