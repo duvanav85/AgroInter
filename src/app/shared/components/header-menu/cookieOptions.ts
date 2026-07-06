6796f9a2c336070bc5ef9581a88ff902f2d7f423
Duvanav85 <duvanav85@yahoo.es>
2026-07-06 11:10:18 -0500

    Update elimina el Env del conf

--- src/app/shared/components/header-menu/cookieOptions.ts
+++ src/app/shared/components/header-menu/cookieOptions.ts
@@ -1,8 +1,8 @@
 import { CookieOptions } from "ngx-cookie-service";
-import { environment } from "../../../../environments/environment";
+//import { environment } from "../../../../environments/environment";
 export const cookieOptions : CookieOptions = {
   path: '/',
-  domain: environment.domain,
+  //domain: environment.domain,
   secure: true,
   sameSite: 'Strict'
 }
